a205685bea6e0c00a40376e115902f08e4b9de37
John Matter <jmatter4@gmail.com>
2025-09-24 17:20:51 -0400

    Add plugin metadata constants for CLAP export

--- examples/clap-plugin/src/clap-saw-demo-entry.cpp
+++ examples/clap-plugin/src/clap-saw-demo-entry.cpp
@@ -2,5 +2,10 @@
 #include "clap-saw-demo-gui.h"
 #include <CLAPExport.h>
 
+// Plugin metadata constants required by CLAPExport.h
+const char* VendorURL = "https://madronalabs.com";
+const char* PluginVersion = "1.0.0";
+const char* PluginDescription = "A simple sawtooth oscillator plugin with GUI";
+
 // One line export replaces entire pluginentry.cpp
 MADRONALIB_EXPORT_CLAP_PLUGIN_WITH_GUI(ClapSawDemo, ClapSawDemoGUI, "Clap Saw Demo", "Madrona Labs")
